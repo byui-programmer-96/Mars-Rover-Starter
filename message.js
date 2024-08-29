77928e6190dfc785f13db6db99a4157195b75666
Cailynn Bambas <30276402+byui-programmer-96@users.noreply.github.com>
2024-08-29 11:06:14 -0500

    Update message.js
    message changes

--- message.js
+++ message.js
@@ -1,5 +1,12 @@
 class Message {
-   // Write code here!
-}
-
-module.exports = Message;
+   constructor(name, commands) {
+     if (typeof name === 'undefined' || name === null) {
+       throw Error('Name required.');
+     }
+     this.name = name;
+     this.commands = commands;
+   }
+ }
+ 
+ module.exports = Message;
+ 
